1843b91fe6f58d897e30b7f9f65d3bfed31b3173
Jorge Estrada <jorgeaestrada45@gmail.com>
2025-05-04 22:02:05 -0700

    Funcionalidad Añadida
    Se añadio la funcionalidad para ver los nombres de quien envia los mensajes.

--- servidor/index.js
+++ servidor/index.js
@@ -2,15 +2,34 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-    //Pedo enviar el archivo asi o con process.cwd() + ruta
-    //res.sendFile(__dirname + '/index.html');
+    res.sendFile(process.cwd() + '/cliente/index.html');
+});
 
-    res.sendFile(process.cwd() + '/cliente/index.html')
+io.on('connection', (socket) => {
+    console.log('Un usuario se ha conectado');
+
+    socket.on('set username', (username) => {
+        socket.username = username;
+        console.log(`Usuario conectado: ${username}`);
+        io.emit('user connected', `${username} se ha unido al chat`);
+    });
+
+    socket.on('chat message', (msg) => {
+        console.log(`Mensaje de ${socket.username}: ${msg}`);
+        io.emit('chat message', { username: socket.username, message: msg });
+    });
+
+    socket.on('disconnect', () => {
+        console.log(`Usuario desconectado: ${socket.username || 'Anónimo'}`);
+        io.emit('user disconnected', `${socket.username || 'Un usuario'} se ha desconectado`);
+    });
 });
 
 server.listen(3000, () => {
-    console.log('Servidor escuchando en el puerto 3000')
-})
+    console.log('Servidor escuchando en el puerto 3000');
+});
 
